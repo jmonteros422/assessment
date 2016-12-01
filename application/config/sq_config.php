c6c1a450ccd1d9699b8b455ba5a2039406d03148
janmonteros <janraymonteros@gmail.com>
2016-12-01 14:38:55 +0800

    Configuration file

--- application/config/sq_config.php
+++ application/config/sq_config.php
@@ -1,7 +1,7 @@
-<?php 
-		$sq_base_url='';
-		$sq_hostname='localhost';
-		$sq_dbname='';
-		$sq_dbusername='root';
-		$sq_dbpassword='';
-		?>
+<?php
+$sq_base_url='http://localhost/assessment/';
+$sq_hostname='localhost';
+$sq_dbname='assessment';
+$sq_dbusername='root';
+$sq_dbpassword='';
+?>
